f18ddc400f2ac6a7c53216ce9364db63d9f02e83
Fauzan Rindra <fauzanrindra@gmail.com>
2024-03-28 16:17:18 +0700

    fix: test isolation

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,21 +1,43 @@
 use std::net::TcpListener;
 
-use email_newsletter::configuration::get_configuration;
-use sqlx::PgPool;
+use email_newsletter::configuration::{get_configuration, DatabaseSettings};
+use sqlx::{Connection, Executor, PgConnection, PgPool};
+use uuid::Uuid;
 
 pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
 }
 
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    // create database
+    let mut connection = PgConnection::connect(&config.connection_string_without_db())
+        .await
+        .expect("Failed to connect to Postgres");
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("Failed to create database");
+
+    // migrate database
+    let connection_pool = PgPool::connect(&config.connection_string())
+        .await
+        .expect("Failed to connect to Postgres pool");
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database");
+
+    connection_pool
+}
+
 async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    let configuration = get_configuration().expect("Failed to read configuration.");
+    let mut configuration = get_configuration().expect("Failed to read configuration.");
+    configuration.database.database_name = Uuid::new_v4().to_string();
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
-    let db_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed connect to database poo");
+    let db_pool = configure_database(&configuration.database).await;
     let server =
         email_newsletter::startup::run(listener, db_pool.clone()).expect("Failed to bind address");
 
