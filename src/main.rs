675d90b6c4408df9f02f276ec62dd1930d762fe1
Fauzan Rindra <fauzanrindra@gmail.com>
2024-03-28 15:43:54 +0700

    feat: database configuration

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use std::net::TcpListener;
 
-use email_newsletter::run;
+use email_newsletter::configuration::get_configuration;
+use email_newsletter::startup::run;
+use sqlx::PgPool;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port 8000");
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address).expect("Failed to bind port 8000");
 
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,26 +1,94 @@
 use std::net::TcpListener;
 
-fn spawn_app() -> String {
+use email_newsletter::configuration::get_configuration;
+use sqlx::PgPool;
+
+pub struct TestApp {
+    pub address: String,
+    pub db_pool: PgPool,
+}
+
+async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let configuration = get_configuration().expect("Failed to read configuration.");
     let port = listener.local_addr().unwrap().port();
-    let server = email_newsletter::run(listener).expect("Failed to bind address");
+    let address = format!("http://127.0.0.1:{}", port);
+    let db_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed connect to database poo");
+    let server =
+        email_newsletter::startup::run(listener, db_pool.clone()).expect("Failed to bind address");
 
     let _ = tokio::spawn(server);
 
-    format!("http://127.0.0.1:{}", port)
+    TestApp { address, db_pool }
 }
 
 #[tokio::test]
 async fn health_check_works() {
-    let address = spawn_app();
+    let app = spawn_app().await;
     let client = reqwest::Client::new();
 
     let response = client
-        .get(&format!("{}/health_check", address))
+        .get(&format!("{}/health_check", app.address))
         .send()
         .await
         .expect("Failed to execute request");
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let body = "name=fauzan%20rindra&email=fauzanrindra%40gmail.com";
+    let response = client
+        .post(format!("{}/subscriptions", &app.address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute subscriptions request");
+
+    assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "fauzanrindra@gmail.com");
+    assert_eq!(saved.name, "fauzan rindra");
+}
+
+#[tokio::test]
+async fn subscribe_return_a_400_when_data_is_missing() {
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=fauzan%20rindra", "missing the email"),
+        ("email=fauzanrindra%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, message) in test_cases {
+        let response = client
+            .post(format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute subscriptions request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            message
+        )
+    }
 }
